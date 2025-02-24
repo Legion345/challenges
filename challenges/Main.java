4f89af014f5fcb9d1d0a134fae0fe172c46f1aa2
Legion345 <harelasaraf@gmail.com>
2025-02-24 13:37:04 -0800

    Updated Code

--- challenges/Main.java
+++ challenges/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
 	public static void main(String[] args) {
 		PrintNum(10);
@@ -9,7 +8,16 @@ public class Main {
 		System.out.println();
 		Factorial(5);
 		System.out.println();
-		Exponentiation(int base, int exponent);
+		Exponent(10, 5);
+		System.out.println();
+		Reversed(123456);
+		System.out.println();
+		int[] numbers = { 1, 2, 3, 4, 5, 6 };
+		EvenOdd(numbers);
+		System.out.println();
+		System.out.println("The number is prime: " + isPrime(12));
+		System.out.println();
+		findHCF(100, 101);
 	}
 
 	public static void PrintNum(int size) {
@@ -50,19 +58,55 @@ public class Main {
 			System.out.println(i + "! = " + fact + " = " + product);
 		}
 	}
-	public static void Exponentiation(int base, int exponent) {
-		Scanner console = new Scanner(System.in);
+
+	public static void Exponent(int base, int exponent) {
 		int value = 1;
-
-		System.out.print("Base: ");
-		base = console.nextInt();
-
-		System.out.print("Exponenet: ");
-		exponent = console.nextInt();
 
 		for (int i = 1; i <= exponent; i++) {
 			value *= base;
 			System.out.println(i + "^" + exponent + " = " + value);
+
 		}
+	}
+
+	public static void Reversed(int sequence) {
+		int rev = 0;
+
+		while (sequence != 0) {
+			int rem = sequence % 10;
+			rev = rev * 10 + rem;
+			sequence = sequence / 10;
+		}
+		System.out.println(rev);
+	}
+
+	public static void EvenOdd(int[] numbers) {
+		int evenSum = 0;
+		int oddSum = 0;
+
+		for (int i = 0; i < numbers.length; i++) {
+			if ((numbers[i] % 2) == 0) {
+				evenSum += numbers[i];
+			} else {
+				oddSum += numbers[i];
+			}
+		}
+		System.out.println("The sum of the even numbers is " + evenSum);
+		System.out.println("The sum of the odd numbers is " + oddSum);
+	}
+
+	public static boolean isPrime(int number) {
+		if (number <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= number / 2; i++) {
+			if ((number % i) == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void findHCF(int first, int second) {
 	}
 }
